87b8906c0c8d71dbd9508355e11a612e92ed6dc0
Juliobxds <theuzaocrossfire@gmail.com>
2024-04-22 06:57:56 -0300

    Inserindo Dados no banco!

--- Moments/src/app/components/pages/new-moment/new-moment.component.ts
+++ Moments/src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Moment } from 'src/app/Moment';
+import { MommentService } from 'src/app/services/momment.service';
+
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
@@ -8,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 export class NewMomentComponent implements OnInit {
   btnText = 'Compartilhar!';
 
-  constructor() { }
+  constructor(private momentService: MommentService) { }
 
   ngOnInit(): void {
+  }
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+
+    // todo
+
+    await this.momentService.createMoment(formData).subscribe();
+
+    // exibir msg
+
+    // redirect
+
   }
 
 }
